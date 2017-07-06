8ffbbd92aedf64948576214faabc53229ffbd106
jeffery shivers <jefferyshivers@gmail.com>
2017-07-05 23:45:19 -0400

    wip test docs

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:51,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to minimum enclosure&#8217;s documentation!"],titleterms:{document:0,enclosur:0,indic:0,minimum:0,tabl:0,welcom:0}})
